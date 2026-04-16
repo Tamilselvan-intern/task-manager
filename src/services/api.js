a3b1ffa29cc801a6ce20a6195781a618c05ae817
Tamil <tamilselvanvenba@gmail.com>
2026-04-16 16:16:11 +0530

    feat:api-limit added

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ const BASE_URL = import.meta.env.VITE_API_URL
 console.log("BASE_URL:", BASE_URL)
 
 export const fetchTasks = async () => {
-  const res = await fetch(`${BASE_URL}/todos?_limit=5`)
+  const res = await fetch(`${BASE_URL}/todos?_page=1&_limit=20`)
   if (!res.ok) {
     throw new Error("Failed to fetch tasks");
   }
