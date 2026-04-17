d098af1b1183ce6d377154bcdda6d45079567875
Tamil <tamilselvanvenba@gmail.com>
2026-04-17 16:34:17 +0530

    port given

--- backend/server.js
+++ backend/server.js
@@ -71,6 +71,8 @@ app.put("/tasks/:id", async (req, res) => {
 });
  
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
